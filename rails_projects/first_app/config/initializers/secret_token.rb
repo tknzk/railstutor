a0942159dccdc7c737aff2f0c0b7a931ef7f479b
tknzk <tkm.knzk@gmail.com>
2014-02-27 15:01:06 +0900

    rails new fisrt_app , bundle update/install

---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'db61046ae841131d3fe8c6f35ab41c9df891cc8f05d10a0816f977484e6e02b53957d7f178a171f2637caa775e33609529db9ecbabba706cdeeda9836a28f750'
